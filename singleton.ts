598ec29bc932308228b1b4797da553e7b065c4a1
Alexander Shestakov <alexandurshestakov@gmail.com>
2026-01-22 12:49:50 +0200

    added an example

--- singleton.ts
+++ singleton.ts
@@ -1,16 +1,25 @@
 class Singleton {
+	public exampleValue: string = "example";
+
 	static #instance: Singleton;
 
 	// Prevent creating new instances from outside
 	private constructor() {}
 
-	public static getInstance(): Singleton {
+	public static get instance(): Singleton {
 		if (!Singleton.#instance) {
 			Singleton.#instance = new Singleton();
 		}
 
 		return Singleton.#instance;
 	}
-
-	// ... other logic
 }
+
+const a = Singleton.instance;
+const b = Singleton.instance;
+
+console.log(b.exampleValue);
+
+a.exampleValue = "changed";
+
+console.log(b.exampleValue); // Outputs: "changed"
